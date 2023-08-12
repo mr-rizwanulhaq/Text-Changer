b9d00c7d8e9987688e277af521adc829383fcaa7
Muhammad Rizwan Ul Haq <rizaqua.6@gmail.com>
2023-08-12 11:52:06 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import './App.css';
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+
+} from "react-router-dom";
+
+import About from './components/About';
+
+
+
+function App() {
+const [mode, setmode] = useState("light")
+const [btnmode, setbtnmode] = useState("dark")
+const [btntext, setbtntext] = useState("Dark Mode")
+const [alert, setAlert] = useState(null)
+
+
+let showAlert=(message, type)=>{
+  setAlert({
+      message: message ,
+     type: type
+  
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    }, 2000)
+}
+
+let toggleMode=()=>{
+  if (mode=== "light") {
+    setmode("dark");
+    setbtnmode("light");
+    setbtntext("Light Mode");
+    document.body.style.backgroundColor ="#5A5A5A";
+    showAlert("Dark mode has been enabled", "success");
+    
+  } else {
+    setmode("light")
+    setbtnmode("dark")
+    setbtntext("Dark Mode")
+    document.body.style.backgroundColor ="white";
+    showAlert("Light mode has been enabled", "success");
+    
+  }
+}
+
+
+  return (
+    <>
+      <BrowserRouter>
+            <Navbar title="Text Changer" tabOne= "Home" mode={mode} toggleMode={toggleMode} btnmode={btnmode} btntext={btntext}/>
+            <Alert alert={alert}/>
+              <div className="container my-3">
+            <Routes>
+              <Route exact path="/about" element={<About mode={mode} />} />
+              <Route exact path="/" element={<TextForm heading ="Enter The Text To Analyze" mode={mode}  btnmode={btnmode} showAlert={showAlert}/>} />
+            </Routes>
+
+              </div>
+
+
+    </BrowserRouter>
+        
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+
+
+export default function About(props) {
+
+
+return(
+    <div className="container" style={{color: props.mode==='dark'?'white':'black'}}><h1 className="text-center my-3" style={{color: props.mode==='dark'?'white':'black'}}>About Our Text Changer Website</h1>
+
+    Welcome to our innovative text Changer Website,
+     where simplicity meets functionality. 
+     Our platform offers a seamless solution for 
+     enhancing your text by converting it into upper
+      and lower case while effortlessly eliminating
+       redundant spaces. Designed with user convenience
+        in mind, our intuitive interface ensures a hassle-free 
+        experience, allowing you to focus on the content itself
+         rather than the formatting. Elevate your text effortlessly 
+         with our tools, and embrace a cleaner, more polished presentation. 
+         Streamline your writing process today with our text changer website.</div>
+)
+   
+}
